0318d9066ca88c5bad5d19a09bf6a48a441c5335
Tera Byte <tubagus.aom.swk@gmail.com>
2022-11-30 16:09:33 +0700

    newrepo

--- application/views/templates/bootstraps/struktur_organisasi_1.php
+++ application/views/templates/bootstraps/struktur_organisasi_1.php
@@ -12,15 +12,43 @@
   <div class="container">
     <div class="row">
 
-      <div class="col-lg-12 mb-lg-0">
+      <div class="col-lg-12 mb-lg-0 mb-lg-0">
         <div class="post-content post-single">
 
-          <h3 class="tb-sub-title">Struktur Organisasi</h3>
+          <h3 class="tb-sub-title">Struktur Organisasi Asosiasi</h3>
 
-          <div class="post-media post-image">
+          <div class="post-media mt-lg-0 post-image">
             <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/sruktur_kimia.png" class="img-fluid" alt="post-image">
           </div>
-          
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_bisnis.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_ekspor.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_informasi.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_makanan.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_mesin.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/strukur_properti.png" class="img-fluid" alt="post-image">
+          </div>
+
+          <div class="post-media mt-lg-0 post-image">
+            <img loading="lazy" src="<?=base_url()?>assets/img/struktur_organisasi/struktur_alb.png" class="img-fluid" alt="post-image">
+          </div>
+
         </div><!-- post content end -->
       </div>
 
