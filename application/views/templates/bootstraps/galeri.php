5c5bade596f7bd8e5249fd569be3d6f766b9c3ac
Tera Byte <tubagus.aom.swk@gmail.com>
2022-12-01 08:39:06 +0700

    galery

--- application/views/templates/bootstraps/galeri.php
+++ application/views/templates/bootstraps/galeri.php
@@ -16,12 +16,6 @@
           <label class="active" for="all">
             <input type="radio" name="shuffle-filter" id="all" value="all" checked="checked">SHOW ALL
           </label>
-          <label for="tkdn">
-            <input type="radio" name="shuffle-filter" id="tkdn" value="tkdn" checked="checked">TKDN
-          </label>
-          <label for="sni">
-            <input type="radio" name="shuffle-filter" id="sni" value="sni">SNI
-          </label>
           <label for="asosiasi">
             <input type="radio" name="shuffle-filter" id="asosiasi" value="asosiasi">ASOSIASI
           </label>
@@ -34,12 +28,275 @@
           <label for="perkumpulan">
             <input type="radio" name="shuffle-filter" id="perkumpulan" value="perkumpulan">PERKUMPULAN
           </label>
+          <label for="tkdn">
+            <input type="radio" name="shuffle-filter" id="tkdn" value="tkdn">TKDN
+          </label>
+          <label for="sni">
+            <input type="radio" name="shuffle-filter" id="sni" value="sni">SNI
+          </label>
         </div>
 
         <div class="row shuffle-wrapper">
           <div class="col-1 shuffle-sizer"></div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;,&quot;healthcare&quot;]">
+          <!-- asosiasi start -->
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a1.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a1.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Blockchain Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a2.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a2.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Blockchain Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a3.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a3.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Blockchain Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a4.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a4.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Blockchain Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a5.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a5.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Blockchain Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a11.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a11.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Nikel Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a22.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a22.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Nikel Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a33.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a33.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Nikel Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a44.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a44.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Nikel Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a55.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a55.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Nikel Indonesia</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a111.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a111.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Pengelolaan Rajungan</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a222.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a222.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Pengelolaan Rajungan</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a333.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a333.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Pengelolaan Rajungan</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a444.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a444.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Pengelolaan Rajungan</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;asosiasi&quot;]">
+            <div class="project-img-container">
+              <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/asosiasi/a555.jpg" aria-label="project-img">
+                <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/asosiasi/a555.jpg" alt="project-img">
+                <span class="gallery-icon"><i class="fa fa-plus"></i></span>
+              </a>
+
+              <div class="project-item-info">
+                <div class="project-item-info-content">
+                  <h3 class="project-item-title">
+                    <a href="projects-single.html">Asosiasi Pengelolaan Rajungan</a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- tkdn start -->
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/tkdn1.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/tkdn1.jpg" alt="project-img">
@@ -58,45 +315,26 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;,&quot;healthcare&quot;]">
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/tkdn2.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/tkdn2.jpg" alt="project-img">
                 <span class="gallery-icon"><i class="fa fa-plus"></i></span>
               </a>
-              <!-- <div class="project-item-info">
-                <div class="project-item-info-content">
-                  <h3 class="project-item-title">
-                    <a href="projects-single.html">Capital Teltway Building</a>
-                  </h3>
-                  <p class="project-cat">
-                    <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                  </p>
-                </div>
-              </div> -->
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;,&quot;healthcare&quot;]">
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;tkdn&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/tkdn3.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/tkdn3.jpg" alt="project-img">
                 <span class="gallery-icon"><i class="fa fa-plus"></i></span>
               </a>
-              <!-- <div class="project-item-info">
-                <div class="project-item-info-content">
-                  <h3 class="project-item-title">
-                    <a href="projects-single.html">Capital Teltway Building</a>
-                  </h3>
-                  <p class="project-cat">
-                    <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                  </p>
-                </div>
-              </div> -->
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;,&quot;healthcare&quot;]">
+          <!-- sni start -->
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/sni1.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/sni1.jpg" alt="project-img">
@@ -115,41 +353,21 @@
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;,&quot;healthcare&quot;]">
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/sni2.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/sni2.jpg" alt="project-img">
                 <span class="gallery-icon"><i class="fa fa-plus"></i></span>
               </a>
-              <!-- <div class="project-item-info">
-                <div class="project-item-info-content">
-                  <h3 class="project-item-title">
-                    <a href="projects-single.html">Capital Teltway Building</a>
-                  </h3>
-                  <p class="project-cat">
-                    <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                  </p>
-                </div>
-              </div> -->
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;,&quot;healthcare&quot;]">
+          <div class="col-lg-4 col-md-6 shuffle-item" data-groups="[&quot;sni&quot;]">
             <div class="project-img-container">
               <a class="gallery-popup" href="<?=base_url()?>assets/img/galeri/sni3.jpg" aria-label="project-img">
                 <img class="img-fluid" src="<?=base_url()?>assets/img/galeri/sni3.jpg" alt="project-img">
                 <span class="gallery-icon"><i class="fa fa-plus"></i></span>
               </a>
-              <!-- <div class="project-item-info">
-                <div class="project-item-info-content">
-                  <h3 class="project-item-title">
-                    <a href="projects-single.html">Capital Teltway Building</a>
-                  </h3>
-                  <p class="project-cat">
-                    <span class="gallery-icon"><i class="fa fa-plus"></i></span>
-                  </p>
-                </div>
-              </div> -->
             </div>
           </div>
 
